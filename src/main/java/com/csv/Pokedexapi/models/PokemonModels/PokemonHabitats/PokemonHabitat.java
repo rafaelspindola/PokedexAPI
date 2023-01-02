4f613a6356957fa64b4754189c4e6fd0c410672f
Rafael Spíndola <rafa.spin95@gmail.com>
2023-01-01 23:42:48 -0300

    Added PokemonHabitats in PokemonModels directory

---
+++ src/main/java/com/csv/Pokedexapi/models/PokemonModels/PokemonHabitats/PokemonHabitat.java
@@ -0,0 +1,13 @@
+package com.csv.Pokedexapi.models.PokemonModels.PokemonHabitats;
+
+import com.csv.Pokedexapi.models.CommonModels.Name;
+
+import java.util.List;
+
+public class PokemonHabitat {
+
+    private int id;
+    private String name;
+    private List<Name> names;
+//    private NamedAPIResource<PokemonSpecies> pokemon_species;
+}
