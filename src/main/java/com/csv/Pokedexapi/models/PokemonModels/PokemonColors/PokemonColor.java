332b689f157692f11b9fe001152d8bf20493adf9
Rafael Spíndola <rafa.spin95@gmail.com>
2023-01-01 23:34:52 -0300

    Added PokemonColors and PokemonLocationAreas in PokemonModels

---
+++ src/main/java/com/csv/Pokedexapi/models/PokemonModels/PokemonColors/PokemonColor.java
@@ -0,0 +1,13 @@
+package com.csv.Pokedexapi.models.PokemonModels.PokemonColors;
+
+import com.csv.Pokedexapi.models.CommonModels.Name;
+
+import java.util.List;
+
+public class PokemonColor {
+
+    private int id;
+    private String name;
+    private List<Name> names;
+//    private NamedAPIResource<PokemonSpecies> pokemon_species;
+}
---
+++ src/main/java/com/csv/Pokedexapi/models/PokemonModels/PokemonLocationAreas/LocationAreaEncounter.java
@@ -0,0 +1,7 @@
+package com.csv.Pokedexapi.models.PokemonModels.PokemonLocationAreas;
+
+public class LocationAreaEncounter {
+
+//    private NamedAPIResource<LocationArea> location_area;
+//    private List<VersionEncounterDetail> version_details;
+}
