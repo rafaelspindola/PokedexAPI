8fdb74b8edfbaff4530e2eed5195652c26eb9bda
Rafael Spíndola <rafa.spin95@gmail.com>
2023-01-07 19:19:07 -0300

    Added MoveAilments to MovesModels directory

---
+++ src/main/java/com/csv/Pokedexapi/models/MovesModels/MoveAilments/MoveAilment.java
@@ -0,0 +1,13 @@
+package com.csv.Pokedexapi.models.MovesModels.MoveAilments;
+
+import com.csv.Pokedexapi.models.CommonModels.Name;
+
+import java.util.List;
+
+public class MoveAilment {
+
+    private int id;
+    private String name;
+//    private NamedAPIResource<Move> moves;
+    private List<Name> names;
+}
