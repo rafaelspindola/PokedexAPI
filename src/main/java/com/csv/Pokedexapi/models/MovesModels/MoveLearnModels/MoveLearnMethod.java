54080fa3d9536482d2c6233336adc5e3ab7aa767
Rafael Spíndola <rafa.spin95@gmail.com>
2023-01-06 23:53:00 -0300

    Added MoveLearnModels to MovesModels directory

--- src/main/java/com/csv/Pokedexapi/models/MovesModels/MoveLearnModels/MoveLearnMethod.java
+++ src/main/java/com/csv/Pokedexapi/models/MovesModels/MoveLearnModels/MoveLearnMethod.java
@@ -1,15 +1,16 @@
-package com.csv.Pokedexapi.models.MovesModels;
+package com.csv.Pokedexapi.models.MovesModels.MoveLearnModels;
 
 import com.csv.Pokedexapi.models.CommonModels.Description;
 import com.csv.Pokedexapi.models.CommonModels.Name;
 
 import java.util.List;
 
-public class MoveTarget {
+public class MoveLearnMethod {
 
     private int id;
     private String name;
     private List<Description> descriptions;
-//    private NamedAPIResource<Move> moves;
     private List<Name> names;
+//    private NamedAPIResource<VersionGroup> version_groups;
+
 }
