24125ea39f714737feddda2c23e22e69c6fa0f3e
Rafael Spíndola <rafa.spin95@gmail.com>
2023-01-08 23:22:37 -0300

    Added MoveBattleStyles to MovesModels directory

---
+++ src/main/java/com/csv/Pokedexapi/models/MovesModels/MoveBattleStyles/MoveBattleStyle.java
@@ -0,0 +1,12 @@
+package com.csv.Pokedexapi.models.MovesModels.MoveBattleStyles;
+
+import com.csv.Pokedexapi.models.CommonModels.Name;
+
+import java.util.List;
+
+public class MoveBattleStyle {
+
+    private int id;
+    private String name;
+    private List<Name> names;
+}
