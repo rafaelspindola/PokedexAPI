ecb783faa6dbb6a387fd7409147e6c61854f13ea
Rafael Spíndola <rafa.spin95@gmail.com>
2023-01-11 22:22:45 -0300

    Added MoveCategories to MovesModels directory

---
+++ src/main/java/com/csv/Pokedexapi/models/MovesModels/MoveCategories/MoveCategory.java
@@ -0,0 +1,13 @@
+package com.csv.Pokedexapi.models.MovesModels.MoveCategories;
+
+import com.csv.Pokedexapi.models.CommonModels.Description;
+
+import java.util.List;
+
+public class MoveCategory {
+
+    private int id;
+    private String name;
+//    private NamedAPIResource<Move> moves;
+    private List<Description> descriptions;
+}
