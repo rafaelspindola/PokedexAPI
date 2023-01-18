48424badb02deceb111f8490a893aefaa545778e
Rafael Spíndola <rafa.spin95@gmail.com>
2023-01-17 23:34:04 -0300

    Added GamesModels into models directory

---
+++ src/main/java/com/csv/Pokedexapi/models/GamesModels/Generations/Generation.java
@@ -0,0 +1,24 @@
+package com.csv.Pokedexapi.models.GamesModels.Generations;
+
+import com.csv.Pokedexapi.models.CommonModels.Name;
+import com.csv.Pokedexapi.models.CommonModels.NamedAPIResource;
+import com.csv.Pokedexapi.models.MovesModels.Moves.Move;
+import com.csv.Pokedexapi.models.PokemonModels.Abilities.Ability;
+import com.csv.Pokedexapi.models.PokemonModels.PokemonSpecies.PokemonSpecies;
+import com.csv.Pokedexapi.models.PokemonModels.Types.Type;
+
+import javax.swing.plaf.synth.Region;
+import java.util.List;
+
+public class Generation {
+
+    private int id;
+    private String name;
+    private NamedAPIResource<Ability> abilities;
+    private List<Name> names;
+    private NamedAPIResource<Region> regions;
+    private NamedAPIResource<Move> moves;
+    private NamedAPIResource<PokemonSpecies> pokemon_species;
+    private NamedAPIResource<Type> types;
+    private NamedAPIResource<VersionGroup> version_group;
+}
